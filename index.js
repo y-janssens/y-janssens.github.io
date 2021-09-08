3b1db05db1363df1dd73a295d59b54aad388d3d0
Srphy <74479083+y-janssens@users.noreply.github.com>
2021-09-08 06:40:57 +0200

    update

--- index.js
+++ index.js
@@ -524,20 +524,41 @@ function submit() {
     document.getElementById("c_mail").value = "";
     document.getElementById("c_msg").value = "";
 
-    Email.send({
-        SecureToken: "ec9d1734-5c56-46be-a914-35fa6286fa1d",
-        To: 'yoann.janssens@live.fr',
-        From: "skorpiostyle@gmail.com",
-        Subject: "Un nouveau message de " + form_name,
-        Body: "Un nouveau message de " + form_name + " vient d'arriver." + "</br>" +
-            "E-mail: " + form_mail + "</br>" + "</br>" +
-            form_msg
-    }).then(
-        $('#submit').css('color', '#4CC417'),
-        document.getElementById('submit').value = "Message envoyé!",
+    if (form_name !== "" || form_mail !== "" || form_msg !== "") {
+        Email.send({
+            SecureToken: "ec9d1734-5c56-46be-a914-35fa6286fa1d",
+            To: 'yoann.janssens@live.fr',
+            From: "skorpiostyle@gmail.com",
+            Subject: "Un nouveau message de " + form_name,
+            Body: "Un nouveau message de " + form_name + " vient d'arriver." + "</br>" +
+                "E-mail: " + form_mail + "</br>" + "</br>" +
+                form_msg
+        }).then(
+            $('#submit').css('color', '#4CC417'),
+            document.getElementById('submit').value = "Message envoyé!",
+            setTimeout(function () {
+                $('#submit').css('color', 'rgba(120, 120, 120, 0.85)');
+                document.getElementById('submit').value = "Envoyer";
+            }, 2000)
+        );
+    } else if (form_name == "" || form_mail == "" || form_msg == "") {
+        document.getElementById("c_name").value = "* Nom";
+        document.getElementById("c_mail").value = "* E-mail";
+        document.getElementById("c_msg").value = "* Message";
+        document.getElementById('submit').value = "Non envoyé";
+        $('#c_name').css('color', '#ec5151');
+        $('#c_mail').css('color', '#ec5151');
+        $('#c_msg').css('color', '#ec5151');
+        $('#submit').css('color', '#ec5151');
         setTimeout(function () {
             $('#submit').css('color', 'rgba(120, 120, 120, 0.85)');
+            $('#c_name').css('color', 'rgba(120, 120, 120, 0.75)');
+            $('#c_mail').css('color', 'rgba(120, 120, 120, 0.75)');
+            $('#c_msg').css('color', 'rgba(120, 120, 120, 0.75)');
             document.getElementById('submit').value = "Envoyer";
+            document.getElementById("c_name").value = "";
+            document.getElementById("c_mail").value = "";
+            document.getElementById("c_msg").value = "";
         }, 2000)
-    );
+    }
 }
